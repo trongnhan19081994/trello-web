4bb42f4c5f1130ed07c7b35eb89bcc64fd520fac
Nhan Do <trongnhan19081994@gmail.com>
2021-07-18 14:58:08 +0700

    update styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,85 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="trello-master">
+      <nav className="navbar app"> App Bar </nav>
+      <nav className="navbar board"> Board Bar </nav>
+      <nav className="board-columns">
+
+        <div className="column">
+          <header> Brainstorm </header>
+          <ul>
+            <li>
+              <img src="https://www.imgonline.com.ua/examples/rainbow-background-2-big.jpg" alt="image title" />
+              Title: Image title
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header> Brainstorm </header>
+          <ul>
+            <li>
+              <img src="https://www.imgonline.com.ua/examples/rainbow-background-2-big.jpg" alt="image title" />
+              Title: Image title
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Brainstorm </header>
+          <ul>
+            <li>
+              <img src="https://www.imgonline.com.ua/examples/rainbow-background-2-big.jpg" alt="image title" />
+              Title: Image title
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Brainstorm </header>
+          <ul>
+            <li>
+              <img src="https://www.imgonline.com.ua/examples/rainbow-background-2-big.jpg" alt="image title" />
+              Title: Image title
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Brainstorm </header>
+          <ul>
+            <li>
+              <img src="https://www.imgonline.com.ua/examples/rainbow-background-2-big.jpg" alt="image title" />
+              Title: Image title
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+      </nav>
     </div>
   );
 }
