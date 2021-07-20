44f7a05ac4fa7500d98dd7ef124656560af701f4
Nhan Do <trongnhan19081994@gmail.com>
2021-07-20 21:33:29 +0700

    update code drag and drop

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.scss';
-import AppBar from 'components/AppBar/AppBar';
-import BoardBar from 'components/BoardBar/BoardBar';
-import BoardContent from 'components/BoardContent/BoardContent';
+import React from 'react'
+import './App.scss'
+import AppBar from 'components/AppBar/AppBar'
+import BoardBar from 'components/BoardBar/BoardBar'
+import BoardContent from 'components/BoardContent/BoardContent'
 
 function App() {
-  return (
-    <div className="trello-master">
+    return (
+        <div className="trello-master">
 
-      <AppBar />
+            <AppBar />
 
-      <BoardBar />
+            <BoardBar />
 
-      <BoardContent />
+            <BoardContent />
 
-    </div>
-  );
+        </div>
+    )
 }
 
-export default App;
+export default App
